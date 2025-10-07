af9098db997e78800329cbad22c893715bd90916
Jeremy Callinan <jeremy.callinan@gmail.com>
2025-10-07 18:53:21 -0400

    After ProjectileLine addition

---
+++ Mission Demo/Assets/ProjectileLine.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class ProjectileLine : MonoBehaviour
+{
+
+    private LineRenderer _line;
+    private bool _drawing = true;
+    private Projectile _projectile;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _line = GetComponent<LineRenderer>();
+        _line.positionCount = 1;
+        _line.SetPosition(0, transform.position);
+        _projectile = GetComponentInParent<Projectile>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (_drawing)
+        {
+            _line.positionCount++;
+            _line.SetPosition(_line.positionCount - 1, transform.position);
+            if (_projectile != null )
+            {
+                if (_projectile.GetComponentInParent<Rigidbody>().IsSleeping())
+                {
+                    _drawing = false;
+                    _projectile = null;
+                }
+            }
+        }
+    }
+}
